557fb650b3de03240c02f6684e1cb91c0f49690d
Tobias Traiser <traiser.tobias@gmail.com>
2022-11-19 20:40:10 +0100

    moving through the scene

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,72 +1,208 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {PointerLockControls} from "three/examples/jsm/controls/PointerLockControls";
 
-const scene = new THREE.Scene()
-
-const camera = new THREE.PerspectiveCamera(
-    150,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-new OrbitControls(camera, renderer.domElement)
-
-
+let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, controls: PointerLockControls;
 const loader = new GLTFLoader();
 
-loader.load( 'models/testingModel.glb', function ( gltf ) {
 
-    scene.add( gltf.scene );
+let moveLeft = false;
+let moveRight = false;
+let moveForward = false;
+let moveBackward = false;
+let canJump = false;
 
-    //gltf.animations; // Array<THREE.AnimationClip>
-    //gltf.scene; // THREE.Group
-    //gltf.scenes; // Array<THREE.Group>
-    //gltf.cameras; // Array<THREE.Camera>
-    //gltf.asset; // Object
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
 
 
-}, undefined, function ( error ) {
+init();
+function init(){
+    camera = new THREE.PerspectiveCamera();
+    scene = new THREE.Scene();
 
-    console.error( error );
+    loader.load( 'models/bar.glb', function ( gltf ) {
+        document.getElementById("info")!.hidden = true
+        document.getElementById("blocker")!.hidden = false
+        scene.add( gltf.scene );
 
-} );
+        //gltf.animations; // Array<THREE.AnimationClip>
+        //gltf.scene; // THREE.Group
+        //gltf.scenes; // Array<THREE.Group>
+        //gltf.cameras; // Array<THREE.Camera>
+        //gltf.asset; // Object
 
-scene.background= new THREE.Color( 0xffffff );
 
-const light = new THREE.PointLight( 0xFFFFFF, 20, 100 );
-light.position.set( 50, 50, 50 );
-scene.add( light );
-/*
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+    }, undefined, function ( error ) {
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)*/
+        console.error( error );
 
-window.addEventListener('resize', onWindowResize, false)
+    } );
+
+
+
+
+    //new OrbitControls(camera, renderer.domElement)
+    camera.position.set(0,5,40)
+    //scene.background= new THREE.Color( 0xffffff );
+
+    const light = new THREE.PointLight( 0xFFFFFF, 20, 100 );
+    light.position.set( 50, 50, 50 );
+    scene.add( light );
+
+
+
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+
+    // moving through the scene
+    controls = new PointerLockControls( camera, document.body );
+
+    const onKeyDown = function ( event: { code: any; } ) {
+
+        switch ( event.code ) {
+
+            case 'ArrowUp':
+            case 'KeyW':
+                moveForward = true;
+                break;
+
+            case 'ArrowLeft':
+            case 'KeyA':
+                moveLeft = true;
+                break;
+
+            case 'ArrowDown':
+            case 'KeyS':
+                moveBackward = true;
+                break;
+
+            case 'ArrowRight':
+            case 'KeyD':
+                moveRight = true;
+                break;
+
+            case 'Space':
+                if ( canJump ) velocity.y += 40;
+                canJump = false;
+                break;
+
+        }
+
+    };
+
+    const onKeyUp = function ( event: { code: any; } ) {
+
+        switch ( event.code ) {
+
+            case 'ArrowUp':
+            case 'KeyW':
+                moveForward = false;
+                break;
+
+            case 'ArrowLeft':
+            case 'KeyA':
+                moveLeft = false;
+                break;
+
+            case 'ArrowDown':
+            case 'KeyS':
+                moveBackward = false;
+                break;
+
+            case 'ArrowRight':
+            case 'KeyD':
+                moveRight = false;
+                break;
+
+        }
+
+    };
+
+    const blocker = document.getElementById( 'blocker' )!;
+    const instructions = document.getElementById( 'instructions' )!;
+
+    instructions.addEventListener( 'click', function () {
+
+        controls.lock();
+
+    } );
+
+    controls.addEventListener( 'lock', function () {
+
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+
+    } );
+
+    controls.addEventListener( 'unlock', function () {
+
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+
+    } );
+    controls.getObject()
+    scene.add( controls.getObject() );
+    document.addEventListener( 'keydown', onKeyDown );
+    document.addEventListener( 'keyup', onKeyUp );
+}
+
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
 
 function animate() {
     requestAnimationFrame(animate)
+    //controls.moveForward(0.01)
+    if ( controls.isLocked ) {
 
-    //cube.rotation.x += 0.01
-    //cube.rotation.y += 0.01
+        const time = performance.now();
+        const delta = ( time - prevTime ) / 1000;
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
 
+        velocity.y -= 9.8 * 10.0 * delta; // 70.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 8.0 * delta;
+        console.log(velocity)
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 8.0 * delta;
+
+
+        //velocity.y = Math.max( 0, velocity.y );
+
+        controls.moveRight(-velocity.x);//* delta
+
+        controls.moveForward(-velocity.z);//* delta
+        controls.getObject().position.y += ( velocity.y * delta );
+
+        if ( controls.getObject().position.y < 5 ) {
+
+            velocity.y = 0;
+            controls.getObject().position.y = 5;
+
+            canJump = true;
+
+        }
+
+        prevTime = time;
+
+    }
     render()
 }
 
